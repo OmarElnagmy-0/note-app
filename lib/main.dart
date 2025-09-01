b5260897e41c467d5714ddd48b8b4ca70dd3406e
OmarElnagmy-0 <omar.elnagmy003@gmail.com>
2025-09-01 04:00:52 +0300

    test git

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
